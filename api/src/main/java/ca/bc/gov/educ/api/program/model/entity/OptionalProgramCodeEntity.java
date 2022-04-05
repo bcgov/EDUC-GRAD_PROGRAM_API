747c24b2fe041d5f17a7b308deeea777cc0d4d12
s.karekkattumanasree <s.karekkattumanasreekumaran@cgi.com>
2022-04-05 09:12:06 -0700

    GRAD2-994

--- api/src/main/java/ca/bc/gov/educ/api/program/model/entity/OptionalProgramCodeEntity.java
+++ api/src/main/java/ca/bc/gov/educ/api/program/model/entity/OptionalProgramCodeEntity.java
@@ -1,23 +1,22 @@
 package ca.bc.gov.educ.api.program.model.entity;
 
-import java.util.Date;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import java.util.Date;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
 @Entity
-@Table(name = "GRADUATION_PROGRAM_CODE")
-public class GraduationProgramCodeEntity extends BaseEntity {
+@Table(name = "OPTIONAL_PROGRAM_CODE")
+public class OptionalProgramCodeEntity extends BaseEntity {
    
 	@Id
-	@Column(name = "GRADUATION_PROGRAM_CODE", nullable = false)
+	@Column(name = "OPTIONAL_PROGRAM_CODE", nullable = false)
     private String programCode;
 	
 	@Column(name = "LABEL", nullable = true)
@@ -35,8 +34,8 @@ public class GraduationProgramCodeEntity extends BaseEntity {
 	@Column(name = "EXPIRY_DATE", nullable = true)
     private Date expiryDate;
 
-	@Column(name = "ASSOCIATED_CREDENTIAL", nullable = true)
-	private String associatedCredential;
+	@Column(name = "ASSOCIATED_CREDENTIALS", nullable = true)
+	private String associatedCredentials;
 
 
 
