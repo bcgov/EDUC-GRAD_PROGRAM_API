224e7c3ab6eacfeaed017d4a83d9b7d401d3a472
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-07-21 10:00:13 -0700

    Updated resource requests

--- api/src/test/java/ca/bc/gov/educ/api/program/EducGradProgramManagementApiApplicationTests.java
+++ api/src/test/java/ca/bc/gov/educ/api/program/EducGradProgramManagementApiApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class EducGradProgramManagementApiApplicationTests {
 
 	//@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 
 }
