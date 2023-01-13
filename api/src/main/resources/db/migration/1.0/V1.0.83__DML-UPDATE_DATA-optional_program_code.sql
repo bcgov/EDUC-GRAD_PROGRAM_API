249e8038d7c10ef9835cc7f8143316e2e4d665e4
Jinil Sung <infstar@gmail.com>
2023-01-13 12:35:39 -0800

    GRAD2-1767 : the minor change is applied.
    GRAD2-1767 : the minor change is applied.

---
+++ api/src/main/resources/db/migration/1.0/V1.0.83__DML-UPDATE_DATA-optional_program_code.sql
@@ -0,0 +1,6 @@
+UPDATE OPTIONAL_PROGRAM_CODE SET
+    DESCRIPTION = 'Students enrolled in Programme francophone who are also working towards a B.C. Certificate of Graduation (English Dogwood).'
+    WHERE OPTIONAL_PROGRAM_CODE = 'DD';
+
+
+
