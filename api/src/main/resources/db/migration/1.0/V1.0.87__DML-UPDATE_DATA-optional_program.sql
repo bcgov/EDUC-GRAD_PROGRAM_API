d321162c1b23fbec85cfb8173a61b9565120b1e6
Kamal Mohammed <kamal.mohammed@outlook.com>
2023-02-06 22:45:21 -0700

    GRAD2-1828 - FLYWAY Changes API Grad Program: OPTIONAL_PROGRAM

---
+++ api/src/main/resources/db/migration/1.0/V1.0.87__DML-UPDATE_DATA-optional_program.sql
@@ -0,0 +1,80 @@
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students on the 2018 B.C. Graduation program who are also reported as participating in the Advanced Placement program.'
+WHERE GRADUATION_PROGRAM_CODE = '2018-EN' AND OPTIONAL_PROGRAM_CODE = 'AD';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students enrolled in Programme francophone on the 2018 Diplôme de fin d''études secondaires en Colombie-Britannique program who are also reported as participating in the Advanced Placement program.'
+WHERE GRADUATION_PROGRAM_CODE = '2018-PF' AND OPTIONAL_PROGRAM_CODE = 'AD';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students enrolled in Programme francophone on the 1986 Diplôme de fin d''études secondaires en Colombie-Britannique program who were also reported as participating in the Advanced Placement program.'
+WHERE GRADUATION_PROGRAM_CODE = '1986-PF' AND OPTIONAL_PROGRAM_CODE = 'AD';
+
+UPDATE OPTIONAL_PROGRAM SET EFFECTIVE_DATE = TIMESTAMP'2023-07-01 00:00:00.0', DESCRIPTION = 'Students on the 2023 B.C. Graduation program who are also reported as participating in the Advanced Placement program.'
+WHERE GRADUATION_PROGRAM_CODE = '2023-EN' AND OPTIONAL_PROGRAM_CODE = 'AD';
+
+UPDATE OPTIONAL_PROGRAM SET EFFECTIVE_DATE = TIMESTAMP'2023-07-01 00:00:00.0', DESCRIPTION = 'Students enrolled in Programme francophone on the 2023 Diplôme de fin d''études secondaires en Colombie-Britannique program who are also reported as participating in the Advanced Placement program.'
+WHERE GRADUATION_PROGRAM_CODE = '2023-PF' AND OPTIONAL_PROGRAM_CODE = 'AD';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students enrolled in Programme francophone on the 1986 Diplôme de fin d''études secondaires en Colombie-Britannique program who were also reported as participating in the International Baccalaureate program as a Certificate candidate.'
+WHERE GRADUATION_PROGRAM_CODE = '1986-PF' AND OPTIONAL_PROGRAM_CODE = 'BC';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students on the 2018 B.C. Graduation program who are also reported as participating in the International Baccalaureate program as a Certificate candidate.'
+WHERE GRADUATION_PROGRAM_CODE = '2018-EN' AND OPTIONAL_PROGRAM_CODE = 'BC';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students enrolled in Programme francophone on the 2018 Diplôme de fin d''études secondaires en Colombie-Britannique program who are also reported as participating in the International Baccalaureate program as a Certificate candidate.'
+WHERE GRADUATION_PROGRAM_CODE = '2018-PF' AND OPTIONAL_PROGRAM_CODE = 'BC';
+
+UPDATE OPTIONAL_PROGRAM SET EFFECTIVE_DATE = TIMESTAMP'2023-07-01 00:00:00.0', DESCRIPTION = 'Students on the 2023 B.C. Graduation program who are also reported as participating in the International Baccalaureate program as a Certificate candidate.'
+WHERE GRADUATION_PROGRAM_CODE = '2023-EN' AND OPTIONAL_PROGRAM_CODE = 'BC';
+
+UPDATE OPTIONAL_PROGRAM SET EFFECTIVE_DATE = TIMESTAMP'2023-07-01 00:00:00.0', DESCRIPTION = 'Students enrolled in Programme francophone on the 2023 Diplôme de fin d''études secondaires en Colombie-Britannique program who are also reported as participating in the International Baccalaureate program as a Certificate candidate.'
+WHERE GRADUATION_PROGRAM_CODE = '2023-PF' AND OPTIONAL_PROGRAM_CODE = 'BC';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students on the 2018 B.C. Graduation program who are also reported as participating in the International Baccalaureate program as a Diploma candidate.'
+WHERE GRADUATION_PROGRAM_CODE = '2018-EN' AND OPTIONAL_PROGRAM_CODE = 'BD';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students enrolled in Programme francophone on the 2018 Diplôme de fin d''études secondaires en Colombie-Britannique program who are also reported as participating in the International Baccalaureate program as a Diploma candidate.'
+WHERE GRADUATION_PROGRAM_CODE = '2018-PF' AND OPTIONAL_PROGRAM_CODE = 'BD';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students enrolled in Programme francophone on the 1986 Diplôme de fin d''études secondaires en Colombie-Britannique program who were also reported as participating in the International Baccalaureate program as a Diploma candidate.'
+WHERE GRADUATION_PROGRAM_CODE = '1986-PF' AND OPTIONAL_PROGRAM_CODE = 'BD';
+
+UPDATE OPTIONAL_PROGRAM SET EFFECTIVE_DATE = TIMESTAMP'2023-07-01 00:00:00.0', DESCRIPTION = 'Students on the 2023 B.C. Graduation program who are also reported as participating in the International Baccalaureate program as a Diploma candidate.'
+WHERE GRADUATION_PROGRAM_CODE = '2023-EN' AND OPTIONAL_PROGRAM_CODE = 'BD';
+
+UPDATE OPTIONAL_PROGRAM SET EFFECTIVE_DATE = TIMESTAMP'2023-07-01 00:00:00.0', DESCRIPTION = 'Students enrolled in Programme francophone on the 2023 Diplôme de fin d''études secondaires en Colombie-Britannique program who are also reported as participating in the International Baccalaureate program as a Diploma candidate.'
+WHERE GRADUATION_PROGRAM_CODE = '2023-PF' AND OPTIONAL_PROGRAM_CODE = 'BD';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students on the 2018 B.C. Graduation program who are also reported as participating in a Career program.'
+WHERE GRADUATION_PROGRAM_CODE = '2018-EN' AND OPTIONAL_PROGRAM_CODE = 'CP';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students enrolled in Programme francophone on the 2018 Diplôme de fin d''études secondaires en Colombie-Britannique program who are also reported as participating in a Career program.'
+WHERE GRADUATION_PROGRAM_CODE = '2018-PF' AND OPTIONAL_PROGRAM_CODE = 'CP';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students enrolled in Programme francophone on the 1986 Diplôme de fin d''études secondaires en Colombie-Britannique program who were also reported as participating in a Career program.'
+WHERE GRADUATION_PROGRAM_CODE = '1986-PF' AND OPTIONAL_PROGRAM_CODE = 'CP';
+
+UPDATE OPTIONAL_PROGRAM SET EFFECTIVE_DATE = TIMESTAMP'2023-07-01 00:00:00.0', DESCRIPTION = 'Students on the 2023 B.C. Graduation program who are also reported as participating in a Career program.'
+WHERE GRADUATION_PROGRAM_CODE = '2023-EN' AND OPTIONAL_PROGRAM_CODE = 'CP';
+
+UPDATE OPTIONAL_PROGRAM SET EFFECTIVE_DATE = TIMESTAMP'2023-07-01 00:00:00.0', DESCRIPTION = 'Students enrolled in Programme francophone on the 2023 Diplôme de fin d''études secondaires en Colombie-Britannique program who are also reported as participating in a Career program.'
+WHERE GRADUATION_PROGRAM_CODE = '2023-PF' AND OPTIONAL_PROGRAM_CODE = 'CP';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students enrolled in Programme francophone on the 2004 Diplôme de fin d''études secondaires en Colombie-Britannique program, who were also working towards the B.C. Certificate of Graduation (English Dogwood).'
+WHERE GRADUATION_PROGRAM_CODE = '2004-PF' AND OPTIONAL_PROGRAM_CODE = 'DD';
+
+UPDATE OPTIONAL_PROGRAM SET LABEL = 'English Dogwood', DESCRIPTION = 'Students enrolled in Programme francophone on the 1986 Diplôme de fin d''études secondaires en Colombie-Britannique program, who were also working towards the B.C. Certificate of Graduation (English Dogwood).'
+WHERE GRADUATION_PROGRAM_CODE = '1986-PF' AND OPTIONAL_PROGRAM_CODE = 'DD';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'Students enrolled in Programme francophone on the 2018 Diplôme de fin d''études secondaires en Colombie-Britannique program, who are also working towards the B.C. Certificate of Graduation (Dual Dogwood).'
+WHERE GRADUATION_PROGRAM_CODE = '2018-PF' AND OPTIONAL_PROGRAM_CODE = 'DD';
+
+UPDATE OPTIONAL_PROGRAM SET LABEL = 'English Dogwood', DESCRIPTION = 'Students enrolled in Programme francophone on the 1996 Diplôme de fin d''études secondaires en Colombie-Britannique program, who were also working towards the B.C. Certificate of Graduation (English Dogwood).'
+WHERE GRADUATION_PROGRAM_CODE = '1996-PF' AND OPTIONAL_PROGRAM_CODE = 'DD';
+
+UPDATE OPTIONAL_PROGRAM SET EFFECTIVE_DATE = TIMESTAMP'2023-07-01 00:00:00.0', DESCRIPTION = 'Students enrolled in Programme francophone on the 2023 Diplôme de fin d''études secondaires en Colombie-Britannique program, who are also working towards the B.C. Certificate of Graduation (Dual Dogwood).'
+WHERE GRADUATION_PROGRAM_CODE = '2023-PF' AND OPTIONAL_PROGRAM_CODE = 'DD';
+
+UPDATE OPTIONAL_PROGRAM SET DESCRIPTION = 'French Immersion students on the 2018 B.C. Graduation program who are also working towards a Diplôme de fin d’études secondaires en Colombie-Britannique.'
+WHERE GRADUATION_PROGRAM_CODE = '2018-EN' AND OPTIONAL_PROGRAM_CODE = 'FI';
+
+UPDATE OPTIONAL_PROGRAM SET EFFECTIVE_DATE = TIMESTAMP'2023-07-01 00:00:00.0', DESCRIPTION = 'French Immersion students on the 2023 B.C. Graduation program who are also working towards a Diplôme de fin d’études secondaires en Colombie-Britannique.'
+WHERE GRADUATION_PROGRAM_CODE = '2023-EN' AND OPTIONAL_PROGRAM_CODE = 'FI';
