e474fb8013dd6cbd4c815b39239e2e9683d628fb
githubmamatha <harmainimamatha@gmail.com>
2023-12-28 10:46:33 -0800

    Grad2-2413 Flyway Change - HD-23438-GRAD - Assessment requirement and not-met messages missing French accents

---
+++ api/src/main/resources/db/migration/1.0/V1.0.100__DML-UPDATE_DATA-optional_program_rqmt_code.sql
@@ -0,0 +1,4 @@
+UPDATE OPTIONAL_PROGRAM_RQMT_CODE SET LABEL = 'Évaluation de littératie 12 – immersion',
+                                      DESCRIPTION = 'Students must write the Évaluation de littératie 12 – immersion' ,
+                                      NOT_MET_DESC = 'No Évaluation de littératie 12 – immersion'
+WHERE OPTIONAL_PROGRAM_RQMT_CODE='203';
