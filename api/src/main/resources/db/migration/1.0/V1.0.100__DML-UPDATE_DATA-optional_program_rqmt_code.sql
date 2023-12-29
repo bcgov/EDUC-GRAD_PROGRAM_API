7a5a2218286a7dc9e73588ef20d180284c821353
githubmamatha <harmainimamatha@gmail.com>
2023-12-29 10:21:45 -0800

    Grad2-2413 Flyway Change - modified script by adding update user and update date.

--- api/src/main/resources/db/migration/1.0/V1.0.100__DML-UPDATE_DATA-optional_program_rqmt_code.sql
+++ api/src/main/resources/db/migration/1.0/V1.0.100__DML-UPDATE_DATA-optional_program_rqmt_code.sql
@@ -1,4 +1,6 @@
 UPDATE OPTIONAL_PROGRAM_RQMT_CODE SET LABEL = 'Évaluation de littératie 12 – immersion',
                                       DESCRIPTION = 'Students must write the Évaluation de littératie 12 – immersion' ,
-                                      NOT_MET_DESC = 'No Évaluation de littératie 12 – immersion'
+                                      NOT_MET_DESC = 'No Évaluation de littératie 12 – immersion' ,
+                                      UPDATE_USER = 'API_GRAD_PROGRAM' ,
+                                      UPDATE_DATE = SYSDATE
 WHERE OPTIONAL_PROGRAM_RQMT_CODE='203';
